7f3bbb4099dddd10bb45ab96a82791bf6bca5dd1
Omar Suriel <omarsuriel@icloud.com>
2022-06-10 14:06:58 -0400

    change userState to use UseReducer

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { fetchProductData } from '../../dataFetching';
 import Layout from '../Layout';
 import ProductDisplay from '../ProductDisplay';
-import Counter from '../Counter';
 
 function HomePage() {
   const [productData, setProductData] = useState([]);
@@ -26,9 +25,6 @@ function HomePage() {
         flexDirection="column"
         alignItems="center"
       >
-        <Box my={6}>
-          <Counter />
-        </Box>
         {productData.map((product) => (
           <Box m={4} key={product.title} width="375px" maxWidth="100%">
             <ProductDisplay product={product} />
